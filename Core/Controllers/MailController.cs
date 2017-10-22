653e0718d38f27fb077d36083ddde958633f67f9
Pawcv <pawcv77@gmail.com>
2017-10-22 23:45:19 +0200

    Added simple mail sending example

---
+++ Core/Controllers/MailController.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+using Core.Models;
+
+// For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace Core.Controllers
+{
+    public class MailController : Controller
+    {
+        // GET: /<controller>/
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult SendMail(string login, string password, string message)
+        {
+            var mailSender = new MailSenderModel(login, password);
+            mailSender.Connect();
+            mailSender.SendMessage(message);
+            mailSender.Disconnect();
+            return RedirectToAction("Index");
+        }
+
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
