8c96ccfbe797c458debf0ff8d9cda397d4368f68
Michał Tomczak <tomczakmihal@gmail.com>
2017-11-12 23:46:26 +0100

    Added Webmail.Data, but now for sure.

---
+++ Webmail.Data/DatabaseInitializer.cs
@@ -0,0 +1,29 @@
+﻿using System.Linq;
+using Webmail.Data.Entities;
+
+namespace Webmail.Data
+{
+    public static class DatabaseInitializer
+    {
+        public static void Initialize(WebmailContext context)
+        {
+            context.Database.EnsureCreated();
+
+            if (context.Users.Any())
+            {
+                return; // dane już są
+            }
+
+            var users = new User[]
+            {
+                new User { Login = "Admin", HashedPassword = User.HashPassword("Admin") },
+                new User { Login = "User1", HashedPassword = User.HashPassword("User1") },
+                new User { Login = "User2", HashedPassword = User.HashPassword("User2") }
+            };
+
+            context.Users.AddRange(users);
+
+            context.SaveChanges();
+        }
+    }
+}
