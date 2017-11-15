8191246d9b3a50b5703955c01b9c4eac850582f4
Michał Tomczak <tomczakmihal@gmail.com>
2017-11-15 22:18:54 +0100

    Added initializing database with one user.

--- Core/Program.cs
+++ Core/Program.cs
@@ -8,7 +8,7 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.DependencyInjection;
-using Webmail.Data;
+using Core.Data;
 
 namespace Core
 {
@@ -17,22 +17,6 @@ namespace Core
         public static void Main(string[] args)
         {
             var host = BuildWebHost(args);
-
-            using (var scope = host.Services.CreateScope())
-            {
-                var services = scope.ServiceProvider;
-                try
-                {
-                    var context = services.GetRequiredService<WebmailContext>();
-                    DatabaseInitializer.Initialize(context);
-                }
-                catch (Exception ex)
-                {
-                    var logger = services.GetRequiredService<ILogger<Program>>();
-                    logger.LogError(ex, "An error occurred while seeding the database.");
-                }
-            }
-
             host.Run();
         }
 
