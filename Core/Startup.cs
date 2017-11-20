a4daaf8d41f93273c2339a2d8990a1de6bf87be0
Pawcv <pawcv77@gmail.com>
2017-11-21 00:17:25 +0100

    Removing unnecessary parameter from ConfigureServices

--- Core/Startup.cs
+++ Core/Startup.cs
@@ -24,7 +24,7 @@ namespace Core
         public IConfiguration Configuration { get; }
 
         // This method gets called by the runtime. Use this method to add services to the container.
-        public void ConfigureServices(IServiceCollection services, IHostingEnvironment env)
+        public void ConfigureServices(IServiceCollection services)
         {
             var connectionString = Environment.GetEnvironmentVariable("DATABASE_CONNECTION_STRING") ?? Configuration.GetConnectionString("DefaultConnection");
 
